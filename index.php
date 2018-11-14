724d5f6401584808d2d71cb4ce47dca7ee6eb0f4
Isabella Palmieri <isabellafelipe@hotmail.com>
2018-11-14 17:28:53 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="pt-br" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <link rel="stylesheet" type="text/css" href="Marcador.css">
+    <!--bootstrap - link cdn -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!--jquer - link cdn -->
+    <script src ="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+    <title> Acesso Gerenciador</title>
+
+    <!--jquer - link cdn -->
+</head>
+
+<body>
+    <!-- Barra de visualização -->
+    <div>
+        <nav class="navbar navbar-default">
+            <div class="container"></div>
+            <div class="navbar navbar-header pull-right ">
+                <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav ml-auto"  > <!--Classe que organiza a lista pra lado a lado-->
+                    <li><a href="#" class="">Home</a></li>
+                    <li><a href="#">Cadastrar Clientes</a></li>
+                    <li><a href="#">Clientes</a></li>
+                </ul>
+                </div>
+            </div>
+
+
+        </nav>
+    </div>
+
+
+</body>
+</html>
